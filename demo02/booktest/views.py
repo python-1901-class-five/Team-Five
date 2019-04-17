5e7a5a211813f49953c98a99d66300046a3ded28
hcy <hcy@qq.com>
2019-04-17 11:59:44 +0800

    添加demo02模块

--- demo02/booktest/views.py
+++ demo02/booktest/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+
+def index(request):
+    print(request)
+    return HttpResponse("首页")
+
+
+def list(request):
+    print(request)
+    return HttpResponse("列表")
