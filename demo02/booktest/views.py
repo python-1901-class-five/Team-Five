6f7e7d5b2ce622d98a5dc0b93c47d131824e5f4d
hcy <hcy@qq.com>
2019-04-18 09:07:53 +0800

    更新demo02模块

--- demo02/booktest/views.py
+++ demo02/booktest/views.py
@@ -1,13 +1,35 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Bookinfo, Heroinfo
+from django.template import loader, RequestContext
 # Create your views here.
 
 
 def index(request):
-    print(request)
-    return HttpResponse("首页")
+    # 加载模板
+    # template = loader.get_template('booktest/index.html')
+    context = {}
+    # res = template.render(context=context)
+    # return HttpResponse(res)
+    return render(request, 'booktest/index.html', context)
 
 
 def list(request):
-    print(request)
-    return HttpResponse("列表")
+    # list1 = []
+    # for i in Bookinfo.objects.all():
+    #     print(i, type(i))
+    #     list1.append(i)
+    # return HttpResponse(list1)
+    # template = loader.get_template('booktest/list.html')
+    booklist = Bookinfo.objects.all()
+    context = {'booklist':booklist}
+    # res = template.render(context=context)
+    # return HttpResponse(res)
+    return render(request, 'booktest/list.html', context)
+
+
+def detail(request, id):
+    book = Bookinfo.objects.get(pk=int(id))
+    context = {'book': book}
+    # return HttpResponse(book)
+    return render(request, 'booktest/detail.html', context)
